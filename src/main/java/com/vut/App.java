884507cf89036d29f94df2dd6142b5a4d2747736
Xavier <xavierkubheka23@gmail.com>
2025-05-20 15:46:58 +0200

    Updated App.java file

--- src/main/java/com/vut/App.java
+++ src/main/java/com/vut/App.java
@@ -1,13 +1,14 @@
 package com.vut;
 
 /**
- * Hello world!
+ * Simple Java Calculator
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Simple java Calculator!" );
     }
 }
+ 
